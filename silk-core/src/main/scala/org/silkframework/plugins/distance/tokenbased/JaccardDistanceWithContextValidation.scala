5584a77a8732208f0627acd24ef7c16a51cdd0e5
joseph6x <jeortizvivar@gmail.com>
2016-06-28 11:15:17 -0500

    flexible names matching

--- silk-core/src/main/scala/org/silkframework/plugins/distance/tokenbased/JaccardDistanceWithContextValidation.scala
+++ silk-core/src/main/scala/org/silkframework/plugins/distance/tokenbased/JaccardDistanceWithContextValidation.scala
@@ -18,7 +18,9 @@ import org.silkframework.entity.Index
 import org.silkframework.rule.similarity.DistanceMeasure
 import org.silkframework.runtime.plugin.{Plugin, Param}
 import edu.ucuenca.authorsrelatedness.Distance
+import uk.ac.shef.wit.simmetrics.similaritymetrics.{SmithWatermanGotoh, JaroWinkler}
 import org.silkframework.util.{GlobalV}
+import scala.collection.mutable.ListBuffer
 
 @Plugin(
   id = "jaccardwv",
@@ -41,7 +43,7 @@ case class JaccardDistanceWithContextValidation(@Param(value = "The maximum sema
        try { 
          var Distance2 = new Distance()
         di=Distance2.NWD(u1,d1,u2,d2,"")
-        System.out.println(u1+"|"+d1+"|"+u2+"|"+d2+"|"+di)
+        System.out.println(u1+"|"+d1+"|"+u2+"|"+d2+"|"+di+" "+dis)
          // Distance2.close();
    } catch {
      case e: Exception => System.out.println("Warn: "+e+e.getStackTraceString)
@@ -56,13 +58,80 @@ case class JaccardDistanceWithContextValidation(@Param(value = "The maximum sema
   
   
   override def apply(values1: Seq[String], values2: Seq[String], limit: Double): Double = {
-    val set1 = values1.toSet
-    val set2 = values2.toSet
+  	
 
-    val intersectionSize = (set1 intersect set2).size
-    val unionSize = (set1 union set2).size
+	var w1 = new ListBuffer[String]()
+	var w2 = new ListBuffer[String]()
 
-    1.0 - intersectionSize.toDouble / unionSize
+
+  	var _values1 = values1.filter ( _.trim.nonEmpty ).distinct
+
+  	var _values2 = values2.filter ( _.trim.nonEmpty ).distinct
+  	var a_ = new SmithWatermanGotoh()
+  	var b_ = new JaroWinkler()
+
+  	var co=0
+
+  	for(  x <- _values1 ){
+  		for(  y <- _values2 ){
+
+  			var xx=x.trim
+  			var yy=y.trim
+
+  			var v = 0.0
+  			
+  			v = b_.getSimilarity(xx,yy)
+  			var exac = v == 1.0
+
+  			var sub=_values1.size < _values2.size
+
+
+  			if ( !sub && xx.startsWith(yy) && yy.length() <=3 || sub && yy.startsWith(xx) && xx.length() <=3   ){
+  				v = 1.0
+  			}else{
+  				v= b_.getSimilarity(xx,yy)
+  			}
+
+  			if (exac){
+  				v = 1.0
+  			}
+
+
+			if (  v>0.99){
+				//System.out.println("Warn: "+xx+"  "+yy+"   "+v)
+				if (w1.contains(xx) || w2.contains(yy))
+				{
+
+				}else	
+				{
+					co = co + 1
+					w1+=xx
+					w2+=yy
+				}
+
+			}  		
+  		}
+  	}
+
+  	val min=Math.min(_values1.size, _values2.size)
+
+
+//System.out.println("Warn2: "+co+"/"+min)
+
+  	var c = 1.0-((co+0.0)/min)
+
+	//System.out.println("Warn2: "+c)
+
+	c
+
+
+    //val set1 = values1.toSet
+    //val set2 = values2.toSet
+
+    //val intersectionSize = (set1 intersect set2).size
+    //val unionSize = (set1 union set2).size
+
+    //1.0 - intersectionSize.toDouble / unionSize
   }
 
   override def index(values: Seq[String], limit: Double) = {
